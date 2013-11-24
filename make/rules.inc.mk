ce405692eaa800f3b386a4c0f847149720c3213a
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-11-24 20:23:50 +0100

    Makefile: Removes folders in output.

--- make/rules.inc.mk
+++ make/rules.inc.mk
@@ -1,6 +1,6 @@
 $(KERNELIMG): $(KERNELELF) $(KERNELLIST)
 	$(MKDIR) $(BINDIR)
-	$(PRINTF) "$(COLOR_WHITE)%-13s$(COLOR_END) %-30s" "Generating" "<$@>..."
+	$(PRINTF) "$(COLOR_WHITE)%-13s$(COLOR_END) %-30s" "Generating" "<$(notdir $@)>..."
 	$(CMD_PREFIX)objcopy $(KERNELELF) -O binary $(KERNELIMG) \
 	$(call errorHandler,$@,$<,genimg)
 	$(ECHO)
@@ -8,21 +8,21 @@ $(KERNELIMG): $(KERNELELF) $(KERNELLIST)
 
 $(KERNELLIST): $(KERNELELF)
 	$(MKDIR) $(MISCDIR)
-	$(PRINTF) "$(COLOR_GEN)%-13s$(COLOR_END) %-30s" "Generating" "<$@>..."
+	$(PRINTF) "$(COLOR_GEN)%-13s$(COLOR_END) %-30s" "Generating" "<$(notdir $@)>..."
 	$(CMD_PREFIX)objdump -D $< > $@ \
 	$(call errorHandler,$@,$<,genlist)
 
 $(KERNELELF): $(OBJ) $(LINKERSCRIPT)
 	$(MKDIR) $(BINDIR)
 	$(MKDIR) $(MISCDIR)
-	$(PRINTF) "$(COLOR_LN)%-13s$(COLOR_END) %-30s" "Linking" "<$@>..."
+	$(PRINTF) "$(COLOR_LN)%-13s$(COLOR_END) %-30s" "Linking" "<$(notdir $@)>..."
 	$(CMD_PREFIX)ld -Map $(MAPFILE) -o $@ -T $(LINKERSCRIPT) $(OBJ) \
 	$(call errorHandler,$@,$<,ld)
 	$(ECHO)
 
 define assemble
 	$(MKDIR) $(OBJDIR)
-	$(PRINTF) "$(COLOR_ASM)%-13s$(COLOR_END) %-30s" "Assembling" "<$2>..."
+	$(PRINTF) "$(COLOR_ASM)%-13s$(COLOR_END) %-30s" "Assembling" "<$(notdir $2)>..."
 	$(CMD_PREFIX)as $(ASM_FLAGS) -o $1 $2 \
 	$(call errorHandler,$1,$2,asm)
 endef
@@ -36,7 +36,7 @@ $(OBJASM): $(OBJDIR)%.o: $$(shell find $(SRCDIR) -name '%.s') $(THIS)
 
 $(ASM): $(ASMDIR)%.s: $(PREDIR)%.i
 	$(MKDIR) $(ASMDIR)
-	$(PRINTF) "$(COLOR_CC)%-13s$(COLOR_END) %-30s" "Compiling" "<$<>..."
+	$(PRINTF) "$(COLOR_CC)%-13s$(COLOR_END) %-30s" "Compiling" "<$(notdir $<)>..."
 	$(CMD_PREFIX)gcc -S $(CC_FLAGS) -o $@ $< \
 	$(call errorHandler,$@,$<,cc)
 
@@ -44,6 +44,6 @@ $(PRE): $(PREDIR)%.i: $$(shell find $(SRCDIR) -name '%.c') $(THIS)
 	$(MKDIR) $(PREDIR)
 	$(MKDIR) $(DEPDIR)
 	$(MKDIR) $(MISCDIR)
-	$(PRINTF) "$(COLOR_PRE)%-13s$(COLOR_END) %-30s" "Preprocessing" "<$<>..."
+	$(PRINTF) "$(COLOR_PRE)%-13s$(COLOR_END) %-30s" "Preprocessing" "<$(notdir $<)>..."
 	$(CMD_PREFIX)gcc -E -o $@ -MMD -MT $@ -MF $(addprefix $(DEPDIR), $(notdir $(<:.c=.d))) $< \
 	$(call errorHandler,$@,$<,pre)
