bcae2727a1a9961073fceac672f6c81939834687
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-11-25 01:23:52 +0100

    Implements some assembly utils for ARM about CPSR.

--- src/kernel_arm.h
+++ src/kernel_arm.h
@@ -1,6 +1,8 @@
 #ifndef _H_KERNEL_ARM
 #define _H_KERNEL_ARM
 
+#include <stdint.h>
+
 #define r0 0
 #define r1 1
 #define r2 2
@@ -22,5 +24,49 @@
 #define sp r13
 #define lr r14
 #define pc r15
+
+#define KERNEL_ARM_MODE_USR 0x10
+#define KERNEL_ARM_MODE_FIQ 0x11
+#define KERNEL_ARM_MODE_IRQ 0x12
+#define KERNEL_ARM_MODE_SVC 0x13
+#define KERNEL_ARM_MODE_ABT 0x17
+#define KERNEL_ARM_MODE_UND 0x18
+#define KERNEL_ARM_MODE_SYS 0x1F
+
+/*
+ * Gets current processor mode
+ * @see KERNEL_ARM_MODE_*
+ */
+uint32_t kernel_arm_get_mode ( );
+
+
+/*
+ * Sets processor mode
+ */
+void kernel_arm_set_mode_usr ( );
+void kernel_arm_set_mode_fiq ( );
+void kernel_arm_set_mode_irq ( );
+void kernel_arm_set_mode_svc ( );
+void kernel_arm_set_mode_abt ( );
+void kernel_arm_set_mode_und ( );
+void kernel_arm_set_mode_sys ( );
+
+
+/*
+ * Gets current CPSR (Status Register) value
+ */
+uint32_t kernel_arm_get_cpsr ( );
+
+
+/*
+ * Enables IRQ in CPSR
+ */
+void kernel_arm_enable_irq ( );
+
+
+/*
+ * Disables IRQ in CPSR
+ */
+void kernel_arm_disable_irq ( );
 
 #endif
