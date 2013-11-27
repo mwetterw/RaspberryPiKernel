10637a589710fb52db02003e818fdd1d9285ca63
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-11-27 01:38:58 +0100

    Lets kernel_main call 'kernel_hardware_init'.

--- src/kernel_main.c
+++ src/kernel_main.c
@@ -1,9 +1,11 @@
 #include "kernel_memory.h"
 #include "kernel_scheduler.h"
+#include "kernel_hardware.h"
 
-void __attribute__((noreturn, naked)) kernel_main ( )
+void __attribute__ ( ( noreturn, naked ) ) kernel_main ( )
 {
 	kernel_memory_init ( );
+	kernel_hardware_init ( );
 	kernel_scheduler_init ( );
 
 	for ( ; ; );
