517c6e9bc66368506468924532f3de4af174c0ce
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-12-01 08:32:00 +0100

    Implements 'kernel_pcb_remove_turnstile'

--- src/kernel/pcb.c
+++ src/kernel/pcb.c
@@ -1,5 +1,8 @@
 #include "pcb.h"
 
+static void
+kernel_pcb_remove_turnstile ( kernel_pcb_t * pcb, kernel_pcb_turnstile_t * turnstile );
+
 void kernel_pcb_turnstile_init ( kernel_pcb_turnstile_t * turnstile )
 {
 	turnstile -> mpFirst = 0;
@@ -19,6 +22,29 @@ void kernel_pcb_add_turnstile ( kernel_pcb_t * pcb, kernel_pcb_turnstile_t * tur
 
 	turnstile -> mpLast -> mpNext = pcb;
 	turnstile -> mpLast = pcb;
+}
+
+void kernel_pcb_remove_turnstile ( kernel_pcb_t * pcb, kernel_pcb_turnstile_t * turnstile )
+{
+    kernel_pcb_t * previous = 0;
+
+    if ( turnstile -> mpFirst == pcb )
+    {
+        turnstile -> mpFirst = pcb -> mpNext;
+    }
+    else
+    {
+        // We get the previous pcb
+        for ( previous = turnstile -> mpFirst ;
+                previous->mpNext != pcb ;
+                previous = previous -> mpNext );
+        previous -> mpNext = pcb -> mpNext;
+    }
+
+    if ( turnstile -> mpLast == pcb )
+    {
+        turnstile -> mpLast = previous;
+    }
 }
 
 void kernel_pcb_turnstile_rotate ( kernel_pcb_turnstile_t * turnstile )
