18c5427409b8c8887e7a5ea1e8bc76521834c0df
Martin Wetterwald <martin.wetterwald@insa-lyon.fr>
2013-12-17 16:26:35 +0100

    Adds comments showing morse code.

--- src/api/led_morse.c
+++ src/api/led_morse.c
@@ -28,43 +28,43 @@ static inline void morse_write_letter ( morse_letter_t morseLetter );
 
 static const morse_letter_t MORSE_CODE [ 36 ] =
 {
-	morse_cast ( 0xa ),		// a
-	morse_cast ( 0x44 ),	// b
-	morse_cast ( 0xa4 ),	// c
-	morse_cast ( 0x23 ),	// d
-	morse_cast ( 0x1 ),		// e
-	morse_cast ( 0x14 ),	// f
-	morse_cast ( 0x33 ),   	// g
-	morse_cast ( 0x4 ),		// h
-	morse_cast ( 0x2 ),		// i
-	morse_cast ( 0x3c ),   	// j
-	morse_cast ( 0x2b ),   	// k
-	morse_cast ( 0x24 ),   	// l
-	morse_cast ( 0x1a ),   	// m
-	morse_cast ( 0x12 ),	// n
-	morse_cast ( 0x3b ),	// o
-	morse_cast ( 0x34 ),   	// p
-	morse_cast ( 0x6c ),   	// q
-	morse_cast ( 0x13 ),   	// r
-	morse_cast ( 0x3 ),		// s
-	morse_cast ( 0x9 ),		// t
-	morse_cast ( 0xb ),		// u
-	morse_cast ( 0xc ),		// v
-	morse_cast ( 0x1b ),   	// w
-	morse_cast ( 0x4c ),   	// x
-	morse_cast ( 0x5c ),   	// y
-	morse_cast ( 0x64 ),   	// z
+	morse_cast ( 0xa ),		// a . _
+	morse_cast ( 0x44 ),	// b _ . . .
+	morse_cast ( 0xa4 ),	// c _ . _ .
+	morse_cast ( 0x23 ),	// d _ . .
+	morse_cast ( 0x1 ),		// e .
+	morse_cast ( 0x14 ),	// f . . _ .
+	morse_cast ( 0x33 ),   	// g _ _ .
+	morse_cast ( 0x4 ),		// h . . . .
+	morse_cast ( 0x2 ),		// i . .
+	morse_cast ( 0x3c ),   	// j . _ _ _
+	morse_cast ( 0x2b ),   	// k _ . _
+	morse_cast ( 0x24 ),   	// l . _ . .
+	morse_cast ( 0x1a ),   	// m _ _
+	morse_cast ( 0x12 ),	// n _ .
+	morse_cast ( 0x3b ),	// o _ _ _
+	morse_cast ( 0x34 ),   	// p . _ _ .
+	morse_cast ( 0x6c ),   	// q _ _ . _
+	morse_cast ( 0x13 ),   	// r . _ .
+	morse_cast ( 0x3 ),		// s . . .
+	morse_cast ( 0x9 ),		// t _
+	morse_cast ( 0xb ),		// u . . _
+	morse_cast ( 0xc ),		// v . . . _
+	morse_cast ( 0x1b ),   	// w . _ _
+	morse_cast ( 0x4c ),   	// x _ . . _
+	morse_cast ( 0x5c ),   	// y _ . _ _
+	morse_cast ( 0x64 ),   	// z _ _ . .
 
-	morse_cast ( 0xfd ),   	// 0
-	morse_cast ( 0x7d ),   	// 1
-	morse_cast ( 0x3d ),   	// 2
-	morse_cast ( 0x1d ),   	// 3
-	morse_cast ( 0xd ),   	// 4
-	morse_cast ( 0x5 ),   	// 5
-	morse_cast ( 0x85 ),   	// 6
-	morse_cast ( 0xc5 ),   	// 7
-	morse_cast ( 0xe5 ),   	// 8
-	morse_cast ( 0xf5 )   	// 9
+	morse_cast ( 0xfd ),   	// 0 _ _ _ _ _
+	morse_cast ( 0x7d ),   	// 1 . _ _ _ _
+	morse_cast ( 0x3d ),   	// 2 . . _ _ _
+	morse_cast ( 0x1d ),   	// 3 . . . _ _
+	morse_cast ( 0xd ),   	// 4 . . . . _
+	morse_cast ( 0x5 ),   	// 5 . . . . .
+	morse_cast ( 0x85 ),   	// 6 _ . . . .
+	morse_cast ( 0xc5 ),   	// 7 _ _ . . .
+	morse_cast ( 0xe5 ),   	// 8 _ _ _ . .
+	morse_cast ( 0xf5 )   	// 9 _ _ _ _ .
 };
 
 void api_led_morse ( const char * string )
--- src/apps/init.c
+++ src/apps/init.c
@@ -1,7 +1,8 @@
 #include "../api/process.h"
-#include "../api/led.h"
+// #include "../api/led.h"
+#include "../api/led_morse.h"
 
-void led_on ( )
+/* void led_on ( )
 {
 	for ( ; ; )
 	{
@@ -15,10 +16,16 @@ void led_off ( )
 	{
 		api_led_off ( );
 	}
-}
+}*/
 
 void init ( )
 {
-	api_process_create ( led_on, 0 );
-	api_process_create ( led_off, 0 );
+	// api_process_create ( led_on, 0 );
+	// api_process_create ( led_off, 0 );
+	for ( ; ; )
+	{
+		api_led_morse ( "git" );
+		api_process_sleep ( 2000000 );
+	}
+
 }
