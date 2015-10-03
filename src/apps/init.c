fa880ebba476136aceb57e3fb432a0fd9cc15e73
Martin Wetterwald <martin@wetterwald.eu>
2015-10-03 16:31:06 +0200

    Cleans init.c

--- src/apps/init.c
+++ src/apps/init.c
@@ -1,31 +1,16 @@
 #include "../api/process.h"
-// #include "../api/led.h"
 #include "../api/led_morse.h"
 
-/* void led_on ( )
+void morse ( )
 {
-	for ( ; ; )
-	{
-		api_led_on ( );
-	}
+    for ( ; ; )
+    {
+		api_led_morse ( "sos" );
+        api_process_sleep ( 2000000 );
+    }
 }
-
-void led_off ( )
-{
-	for ( ; ; )
-	{
-		api_led_off ( );
-	}
-}*/
 
 void init ( )
 {
-	// api_process_create ( led_on, 0 );
-	// api_process_create ( led_off, 0 );
-	for ( ; ; )
-	{
-		api_led_morse ( "git" );
-		api_process_sleep ( 2000000 );
-	}
-
+	api_process_create ( morse, 0 );
 }
