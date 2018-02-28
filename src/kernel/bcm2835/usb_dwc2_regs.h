fca0c6e8688d876ed7b1a5d20df090ba826bea64
Martin Wetterwald <martin@wetterwald.eu>
2018-02-28 22:02:03 +0100

    [dwc2] Add Host Mode registers definition

--- src/kernel/bcm2835/usb_dwc2_regs.h
+++ src/kernel/bcm2835/usb_dwc2_regs.h
@@ -37,12 +37,43 @@ struct dwc2_regs_core
     uint32_t hptxfsiz;          // 0x00100 Host Periodic Transmit FIFO Size Register
     uint32_t dieptxfn [ 15 ];   // 0x00104 -> 0x0013f Device IN Endpoint Transmit FIFO Size Register
 
-    unsigned char reserved2 [ 0x2c0 ];
+    unsigned char reserved2 [ 0x2C0 ]; // 0x00140 -> 0x003FF
+};
+
+// Host Channel-Specific Registers
+struct dwc2_regs_host_hc
+{
+    uint32_t hcchar;    // Host Channel Characteristics Register
+    uint32_t hcsplt;    // Host Channel Split Control Register
+    uint32_t hcint;     // Host Channel Interrupt Register
+    uint32_t hcintmsk;  // Host Channel Interrupt Mask Register
+    uint32_t hcsiz;     // Host Channel Transfer Size Register
+    uint32_t hcdma;     // Host Channel DMA Address Register
+    uint32_t reserved1; // Is this just after hcdmab?
+    uint32_t hcdmab;    // Host Channel DMA Buffer Address Register
 };
 
 // Host Mode CSRs
 struct dwc2_regs_host
 {
+    uint32_t hcfg;      // 0x00400 Host Configuration Register
+    uint32_t hfir;      // 0x00404 Host Frame Interval Register
+    uint32_t hfnum;     // 0x00408 Host Frame Number / Frame Time Remaining Register
+    uint32_t reserved1; // 0x0040C
+    uint32_t hptxsts;   // 0x00410 Host Periodic Transmit FIFO / Queue Status Register
+    uint32_t haint;     // 0x00414 Host All Channels Interrupt Register
+    uint32_t haintmsk;  // 0x00418 Host All Channels Interrupt Mask Register
+    uint32_t hflbaddr;  // 0x0041C Host Frame List Base Address Register
+
+    unsigned char reserved2 [ 0x20 ];
+
+    uint32_t hprt;      // 0x00440 Host Port Control and Status Register
+
+    unsigned char reserved3 [ 0xBC ];
+
+    struct dwc2_regs_host_hc hc [ 16 ]; // 0x00500 -> 0x006FF Host Channel-Specific Registers
+
+    unsigned char reserved4 [ 0x100 ];  // 0x00700 -> 0x007FF
 };
 
 // Device Mode CSRs
@@ -53,21 +84,21 @@ struct dwc2_regs_device
 // Power and Clock Gating CSRs
 struct dwc2_regs_pcg
 {
-    uint32_t pcgcctl; // 0x00e00
-    unsigned char reserved [ 0x1fc ]; // 0x00e04 -> 0x00fff
+    uint32_t pcgcctl; // 0x00E00
+    unsigned char reserved [ 0x1FC ]; // 0x00E04 -> 0x00FFF
 };
 
 struct dwc2_regs
 {
     // Control and Status Registers (CSRs)
-    struct dwc2_regs_core core;             // 0x00000 -> 0x003ff Core Global CSRs
-    struct dwc2_regs_host host;             // 0x00400 -> 0x007ff Host Mode CSRs
-    struct dwc2_regs_device device;         // 0x00800 -> 0x00dff Device Mode CSRs
-    struct dwc2_regs_pcg pcg;               // 0x00e00 -> 0x00fff Power and Clock Gating CSRs
+    struct dwc2_regs_core core;             // 0x00000 -> 0x003FF Core Global CSRs
+    struct dwc2_regs_host host;             // 0x00400 -> 0x007FF Host Mode CSRs
+    struct dwc2_regs_device device;         // 0x00800 -> 0x00DFF Device Mode CSRs
+    struct dwc2_regs_pcg pcg;               // 0x00E00 -> 0x00FFF Power and Clock Gating CSRs
 
-    unsigned char fifos [ 16 ] [ 0x1000 ];  // 0x01000 -> 0x10fff Device Endpoints / Host Channels FIFOs
-    unsigned char reserved [ 0xf000 ];      // 0x11000 -> 0x1ffff
-    unsigned char dfifodbg [ 0x20000 ];     // 0x20000 -> 0x3ffff Direct Access to Data FIFO RAM for Debugging
+    unsigned char fifos [ 16 ] [ 0x1000 ];  // 0x01000 -> 0x10FFF Device Endpoints / Host Channels FIFOs
+    unsigned char reserved [ 0xF000 ];      // 0x11000 -> 0x1FFFF
+    unsigned char dfifodbg [ 0x20000 ];     // 0x20000 -> 0x3FFFF Direct Access to Data FIFO RAM for Debugging
 };
 
 #endif
