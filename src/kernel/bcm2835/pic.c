ff70fba761bd1aa69e6bdd932129f3e194d7bb59
Martin Wetterwald <martin@wetterwald.eu>
2018-03-01 22:32:29 +0100

    Cache enabled interrupts

--- src/kernel/bcm2835/pic.c
+++ src/kernel/bcm2835/pic.c
@@ -4,18 +4,37 @@
 static volatile struct pic * pic =
     ( volatile struct pic * ) PIC_BASE;
 
+// Transform absolute IRQ number to 0 or 1 (register offset)
+#define irq_reg ( irq >> 5 )
+
+// Transform absolute IRQ number to a relative one (irq % 32)
+#define irq_rel ( irq & 31 )
+
+// This is our internal private cache of currently enabled interrupts
+static uint64_t irq_mask;
+
 void pic_enable_irq ( int irq )
 {
-    ( & ( pic -> enable1 ) ) [ irq >> 5 ] = ( 1 << ( irq & 31 ) );
+    if ( ! ( irq_mask  & ( 1 << irq ) ) )
+    {
+        irq_mask |= ( 1 << irq );
+        ( & ( pic -> enable1 ) ) [ irq_reg ] = ( 1 << irq_rel );
+    }
 }
 
 void pic_disable_irq ( int irq )
 {
-    ( & ( pic -> disable1 ) ) [ irq >> 5 ] = ( 1 << ( irq & 31 ) );
+    if ( irq_mask & ( 1 << irq ) )
+    {
+        irq_mask &= ~( 1 << irq );
+        ( & ( pic -> disable1 ) ) [ irq_reg ] = ( 1 << irq_rel );
+    }
 }
 
 void pic_disable_all_interrupts ( )
 {
+    irq_mask = 0;
+
     pic -> disable_basic = 0xff;
     pic -> disable1 = ~0;
     pic -> disable2 = ~0;
@@ -24,7 +43,7 @@ void pic_disable_all_interrupts ( )
 
 int pic_irq_pending ( int irq )
 {
-    return ( ( & ( pic -> pending1 ) ) [ irq >> 5 ] ) & ( 1 << ( irq & 31 ) );
+    return ( ( & ( pic -> pending1 ) ) [ irq_reg ] ) & ( 1 << irq_rel );
 }
 
 void * irq_dispatch ( void * oldSP )
