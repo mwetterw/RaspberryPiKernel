79b96782c1be82f744b421108589955861ca8079
Martin Wetterwald <martin@wetterwald.eu>
2018-03-29 22:18:03 +0200

    [libc] Implement memset

--- src/libc/string.c
+++ src/libc/string.c
@@ -12,3 +12,16 @@ void * memcpy ( void * dest, const void * src, size_t n )
 
     return dest;
 }
+
+void * memset ( void * s, int c, size_t n )
+{
+    unsigned char * d = s;
+    unsigned char ch = c;
+
+    for ( size_t i = 0 ; i < n ; ++i )
+    {
+        d [ i ] = ch;
+    }
+
+    return s;
+}
--- src/libc/string.h
+++ src/libc/string.h
@@ -4,5 +4,6 @@
 typedef unsigned long size_t;
 
 void * memcpy ( void * dest, const void * src, size_t n );
+void * memset ( void * s, int c, size_t n);
 
 #endif
