a8160612b9ffaefa628e97e8be7ace897059767c
Martin Wetterwald <martin@wetterwald.eu>
2018-04-09 13:39:03 +0200

    [libc] Use stddef.h for size_t declaration

--- src/libc/string.h
+++ src/libc/string.h
@@ -1,7 +1,7 @@
 #ifndef _H_LIB_STRING
 #define _H_LIB_STRING
 
-typedef unsigned long size_t;
+#include <stddef.h>
 
 void * memcpy ( void * dest, const void * src, size_t n );
 void * memset ( void * s, int c, size_t n);
